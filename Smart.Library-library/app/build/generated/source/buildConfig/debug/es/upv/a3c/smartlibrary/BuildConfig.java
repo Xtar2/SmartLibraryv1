099e2b8eb4ceb7172c04dc2f6e1689d71971c17f
Xtar2 <Alberto2alonso2001@gmail.com>
2022-11-06 22:47:11 +0100

    Apk2

---
+++ Smart.Library-library/app/build/generated/source/buildConfig/debug/es/upv/a3c/smartlibrary/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package es.upv.a3c.smartlibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "es.upv.a3c.smartlibrary";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
